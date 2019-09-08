2e12befae1fc6dd9f6b7cb1f96ea2c9a11da634a
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-08 16:03:22 +0430

    Added: FSM, reg

--- src/Timer.v
+++ src/Timer.v
@@ -7,9 +7,9 @@ module Timer (
     );
     
     input [2:0] state;
-    output sig_Full;
-    output sig_Temperature;
-    output sig_Completed;
+    output reg sig_Full;
+    output reg sig_Temperature;
+    output reg sig_Completed;
 
     reg [1:0] fill_Water_Counter;
     reg [1:0] heat_Water_Counter;
@@ -23,21 +23,35 @@ module Timer (
     parameter STATE_RINSE = 3'd5;
     parameter STATE_SPIN = 3'd6;
 
+
     always @ (posedge clock) begin 
         case (state)
             STATE_FILL_WATER: begin
+                fill_Water_Counter = fill_Water_Counter + 1'd1;
                 
             end
             STATE_HEAT_WATER: begin
+                heat_Water_Counter = heat_Water_Counter + 1'd1;
                 
             end
             STATE_WASH: begin
+                wash_Counter = wash_Counter + 1'd1;
                 
             end
             STATE_RINSE: begin
+                rinse_Counter = rinse_Counter + 1'd1;
                 
             end
             STATE_SPIN: begin
+                spin_Counter = spin_Counter + 1'd1;
+                
+            end
+            default : begin
+                fill_Water_Counter = 0;
+                heat_Water_Counter = 0;
+                wash_Counter = 0;
+                rinse_Counter = 0;
+                spin_Counter = 0;
                 
             end
 
