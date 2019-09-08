00a41c4defbcffd4f982f6165fa8e0eb1ea2310c
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-08 15:39:37 +0430

    Added: regs, parameters

--- src/Timer.v
+++ src/Timer.v
@@ -1,15 +1,35 @@
 module Timer (
-	state,
-	sig_Full,
-	sig_Temperature,
-	sig_Completed
-	);
-	
-	input [2:0] state;
-	output sig_Full;
-	output sig_Temperature;
-	output sig_Completed;
-	
+    clock,
+    state,
+    sig_Full,
+    sig_Temperature,
+    sig_Completed
+    );
+    
+    input [2:0] state;
+    output sig_Full;
+    output sig_Temperature;
+    output sig_Completed;
+
+    reg [1:0] fill_Water_Counter;
+    reg [1:0] heat_Water_Counter;
+    reg [2:0] wash_Counter;
+    reg [1:0] rinse_Counter;
+    reg [1:0] spin_Counter;
+
+    parameter STATE_FILL_WATER = 3'd2;
+    parameter STATE_HEAT_WATER = 3'd3;
+    parameter STATE_WASH = 3'd4;
+    parameter STATE_RINSE = 3'd5;
+    parameter STATE_SPIN = 3'd6;
+
+    always @ (posedge clock) begin 
+        case (state)
+
+    end                    
+
+
+    
 
 
 
