493b380ad8bef2bc422afc5aa8b27fa5344fed92
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-08 15:30:30 +0430

    Added: Timer.v

---
+++ src/Timer.v
@@ -0,0 +1,17 @@
+module Timer (
+	state,
+	sig_Full,
+	sig_Temperature,
+	sig_Completed
+	);
+	
+	input [2:0] state;
+	output sig_Full;
+	output sig_Temperature;
+	output sig_Completed;
+	
+
+
+
+
+endmodule
